1ed58385cf77eabe348eb5ff3f5d351dc8167f3c
Tunde Akinyanmi <tundebabzy@gmail.com>
2012-12-14 13:30:25 +0100

    initial commit

---
+++ smsing/backends/base.py
@@ -0,0 +1,27 @@
+class BaseBackend(object):
+    """
+    Base class for backend implementations.
+
+    This should be overridden to create backend implementations.
+    """
+    def __init__(self, fail_silently=False, **kwargs):
+        self.fail_silently = fail_silently
+
+    def get_username(self):
+        """
+        Returns username for backend authentication where applicable
+        """
+        pass
+
+    def get_password(self)
+        """
+        Returns username for backend authentication where applicable
+        """
+        pass
+        
+    def send_messages(self, messages):
+        """
+        Sends one or more Sms objects and should return the number
+        sent.
+        """
+        raise NotImplementedError
