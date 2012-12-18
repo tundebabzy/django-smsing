fd75c601d53bcbfe0388075fd5f327db8c22c4ea
Tunde Akinyanmi <tundebabzy@gmail.com>
2012-12-18 12:17:45 +0100

    added code to sanitise supplied arguments.

--- smsing/backends/console.py
+++ smsing/backends/console.py
@@ -11,29 +11,52 @@ import sys
 import threading
 
 from smsing.backends.base import BaseBackend
+from smsing.messaging import Message 
 
 class SmsBackend(BaseBackend):
     def __init__(self, *args, **kwargs):
         self.stream = kwargs.pop('stream', sys.stdout)
         self._lock = threading.RLock()
         super(SmsBackend, self).__init__(*args, **kwargs)
-
+        
     def send_messages(self, messages):
         """Write all messages to the stream in a thread-safe way."""
         if not messages:
             return
         self._lock.acquire()
+
+        # For a single Message object
+        if isinstance(messages, Message):
+            messages = [messages]
+
+        # At least lets make sure we are being supplied with a list. We
+        # can make sure the list contains Message objects later.
+        else:
+            assert isinstance(messages, list), 'If you are trying to send \
+            a single SMS, supply a Message object else supply a list \
+            (or tuple) of Message objects'
+
+        sending_errors = 0
+            
         try:
             # The try-except is nested to allow for
             # Python 2.4 support (Refs #12147)
             try:
                 stream_created = self.open()
                 for message in messages:
-                    self.stream.write(render_message(message))
-                    self.stream.write('\n')
-                    self.stream.write('-'*79)
-                    self.stream.write('\n')
-                    self.stream.flush()  # flush after each message
+                    # Check each element in the list to make sure its
+                    # a Message object.
+                    if isinstance(message, Message) and message.to:
+                        self.stream.write(render_message(message))
+                        self.stream.write('\n')
+                        self.stream.write('-'*79)
+                        self.stream.write('\n')
+                        self.stream.flush()  # flush after each message
+                    else:
+                        self.stream.write('Message sending failed. Invalid message detected.')
+                        self.stream.write('\n')
+                        self.stream.flush()  # flush after each message
+                        sending_errors += 1
                 if stream_created:
                     self.close()
             except:
@@ -41,7 +64,7 @@ class SmsBackend(BaseBackend):
                     raise
         finally:
             self._lock.release()
-        return len(messages)
+        return len(messages) - sending_errors
 
 def render_message(message):
     return u"""to: %(to)s\n%(text)s""" % {
