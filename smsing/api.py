31f8f7441ca4aca191172505a08803bbad390595
Tunde Akinyanmi <tundebabzy@gmail.com>
2012-12-18 11:57:47 +0100

    added functions to send multiple Message objects.

--- smsing/api.py
+++ smsing/api.py
@@ -23,15 +23,46 @@ def get_connection(path=None, fail_silently=False, **kwargs):
 
     return klass(fail_silently=fail_silently, **kwargs)
 
-def send_sms(text, to, fail_silently=False,
-             username=None, password=None, connection=None):
-    """
-    A wrapper over Message.send to send an SMS.
-    """
-    from smsing.messaging import Message
+def _connection(fail_silently=None, username=None, password=None,
+                connection=None):
     connection = connection or get_connection(
         fail_silently = fail_silently,
         username = username,
         password = password,
     )
-    return Message(text=text, to=to, connection=connection).send()
+    return connection
+    
+
+def send_sms(text, to, fail_silently=False,
+             username=None, password=None, connection=None):
+    """
+    This uses the backend to send a single SMS.
+    
+    This should be used for a single message. For bulk messages, use
+    `send_bulk_sms`
+    """
+    if not to:
+        # We are not going to send a message to nobody so just fail
+        return 0
+    from smsing.messaging import Message
+    connection = _connection(fail_silently, username, password, connection)
+    message = Message(text=text, to=to)
+    connection.send_messages(message)
+
+def send_bulk_sms(datatuple, fail_silently=False,
+             username=None, password=None, connection=None):
+    """
+    This uses the backend to send multiple messages.
+    
+    `datatuple` should be in the form: (text, to)
+    eg datatuple::('What time is it? Call me', '0810').
+
+    This should be used for bulk messages. For just a single message,
+    use `send_sms`.
+    """
+
+    from smsing.messaging import Message
+    connection = _connection(fail_silently, username, password, connection)
+    messages = [Message(text=text, to=to)
+                for to, text in datatuple]
+    connection.send_messages(messages)
